37e092e7d73d0397ecf9b3e91f9c3ab8192e9f9e
Maxime Lalisse <maxime.lalisse@gmail.com>
2024-06-02 16:57:23 +0200

    Deployement: Use prefix only in production

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
 import { defineConfig } from "vite";
 import melangePlugin from "vite-plugin-melange";
 
-export default defineConfig({
-  base: "/belenios-mel-verifier/",
-  plugins: [
-    melangePlugin({
-      emitDir: "src",
-      buildCommand: "opam exec -- dune build @app",
-      watchCommand: "opam exec -- dune build --watch @app",
-    }),
-  ],
-  server: {
-    watch: {
-      awaitWriteFinish: {
-        stabilityThreshold: 500,
-        pollInterval: 20,
+export default defineConfig(({ command, mode }) => {
+  return {
+    base: mode === 'production' ? '/belenios-mel-verifier/' : '/',
+
+    plugins: [
+      melangePlugin({
+        emitDir: "src",
+        buildCommand: "opam exec -- dune build @app",
+        watchCommand: "opam exec -- dune build --watch @app",
+      }),
+    ],
+    server: {
+      watch: {
+        awaitWriteFinish: {
+          stabilityThreshold: 500,
+          pollInterval: 20,
+        },
       },
     },
-  },
+  }
 });
